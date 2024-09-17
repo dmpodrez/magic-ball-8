c26f25dca021c5b9d5fd54a999cbd7ca333bfbc0
Dmitry Podrez <dima.padrez.00@icloud.com>
2024-09-17 11:46:43 +0200

    init: creat a file with function(give a random num)

---
+++ magic_code.py
@@ -0,0 +1,33 @@
+import random
+
+answers = [
+    "It is certain",
+    "It is decided",
+    "No doubts",
+    "Definitely yes",
+    "You can be sure of it",
+    "I think - yes",
+    "Most likely",
+    "Good prospects",
+    "Signs point to - yes",
+    "Yes",
+    "Unclear, try again",
+    "Ask later",
+    "Better not to tell you",
+    "Cannot predict now",
+    "Focus and ask again",
+    "According to my data - no",
+    "Very doubtful",
+    "Prospects aren't very good",
+    "My answer is - no",
+    "Don't even think about it",
+]
+
+def random_num():
+  number = random.randint(0,19)
+  return number
+
+
+
+
+
