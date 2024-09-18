8b4ff29728fa63a9dfa33abcf5ba70cfc87ebd75
Dmitry Podrez <dima.padrez.00@icloud.com>
2024-09-18 09:16:03 +0200

    modified:   a function can ask again a question

--- magic_code.py
+++ magic_code.py
@@ -27,16 +27,30 @@ def random_num():
   number = random.randint(0,19)
   return number
 
+print('Hello World, I am the magic ball, and I know the answer to any of your questions.')
+print('What is your name ?')
+name = input()
+print('Hello,', name)
+
 def magic(num):
-  print('Hello World, I am the magic ball, and I know the answer to any of your questions.')
-  print('What is your name ?')
-  name = input()
-  print('Hello,', name)
   print('Ask question')
   while True:
     question = input()
-    print(question)
     print(answers[num])
+    print('Do you want to ask again ?')
+    answer = input()
+    if answer.lower() == 'yes':
+      num = random_num()
+      magic(num)
+    else:
+      print('Have a nice day', name)
+      break
+
+num = random_num()
+magic(num)
+       
+       
+
 
 
 
